fad169edcb0373a3ce7a302fd123ee58efb78ea8
shafrazi <shafrazi@live.com>
2020-01-13 23:13:25 +0530

    add airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+airport_data = CSV.parse(File.read("us-airports.csv"), headers: true)
+airport_data.delete(0)
+codes_arr = []
+airport_data.by_col[13].each do |i|
+  codes_arr << i if i
+end
+
+codes_arr.each do |code|
+  Airport.create(name: code)
+end
